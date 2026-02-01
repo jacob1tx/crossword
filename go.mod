74c3bf1d803d1b2d252bafcc4d86945174dc0d86
jacob1tx <jacob1tx@gmail.com>
2026-02-01 17:41:56 -0600

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module crossword
+
+go 1.25.6
+
+require github.com/jacob1tx/crossword v0.0.0-20260201233513-107f03c1b6f8
--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/jacob1tx/crossword/internal/game"
+	"github.com/jacob1tx/crossword/internal/board"
 )
 
 func main() {
 	fmt.Println("hello world")
+	fmt.Println(board.get(0,0))
 }
