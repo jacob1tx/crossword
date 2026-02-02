33c6a95a78bb8bebef87289b1102bdd6f6399959
jacob1tx <jacob1tx@gmail.com>
2026-02-01 19:58:27 -0600

    add logging, flush out game board

--- internal/board/board.go
+++ internal/board/board.go
@@ -3,38 +3,70 @@ package board
 import (
 	"errors"
 	"fmt"
+
+	"github.com/rs/zerolog/log"
 )
-		
-const width int = 5
-const height int = 5
 
-var board [width][height]rune
+const width uint8 = 5
+const height uint8 = 5
 
-func set(x int, y int, letter rune) (err error) {
+// Special Tiles
+//
+// The correct way to handle this would probably be enums.
+// However, to put the letters and the square types in the same
+// array, I would need to enumerate all of the letters, and that
+// feels bad. Thus, I am "dumb enum-ing" these special tiles
+// as constants.
+
+const (
+	MT uint8 = iota	// Empty
+	DL 				// Double Letter
+	DW 				// Double Word
+	TL 				// Triple Letter
+	TW 				// Triple Word
+	ST 				// Starting Square
+)
+
+// board defines the game board
+// It is initialized with theo
+var board = [width][height]uint8{
+	{MT, TW, MT, TW, MT},
+	{TW, MT, MT, MT, TW},
+	{MT, TW, ST, TW, MT},
+	{TW, MT, MT, MT, TW},
+	{MT, TW, MT, TW, MT},
+}
+
+func Set(x uint8, y uint8, letter uint8) (err error) {
 	errs := validateArrayInputs(x, y)
 	if errs != nil {
 		return errs
 	}
+
 	board[x][y] = letter
+	
+	log.Info().Msgf("[%d,%d] = %c", x, y, letter)
+	log.Debug().Msgf("%v", board)
+
 	return nil
 }
 
-func get(x int, y int) (letter rune, err error) {
+func Get(x uint8, y uint8) (letter uint8, err error) {
 	errs := validateArrayInputs(x, y)
 	if errs != nil {
-		return -1, errs
+		return MT, errs
 	}
 	return board[x][y], nil
 }
 
-func validateArrayInputs(x int, y int) (error) {
+func validateArrayInputs(x uint8, y uint8) (error) {
 	var errs error
 
-	if x >= width || x < 0 {
-		errs = errors.Join(fmt.Errorf("x, %d, must satisfy 0 <= x < width (%d).", x, width), errs)
+	if x >= width {
+		errs = errors.Join(fmt.Errorf("x, %d, must satisfy x < width (%d).", x, width), errs)
 	}
-	if y >= height || y < 0 {
-		errs = errors.Join(fmt.Errorf("y, %d, must satisfy 0 <= y < height (%d).", y, height), errs)
+	if y >= height {
+		errs = errors.Join(fmt.Errorf("y, %d, must satisfy y < height (%d).", y, height), errs)
 	}
 
 	return errs
--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"github.com/jacob1tx/crossword/internal/board"
+	"crossword/internal/board"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	fmt.Println("hello world")
-	fmt.Println(board.get(0,0))
+	debug := flag.Bool("debug", false, "sets log level to debug")
+
+	flag.Parse()
+
+	if *debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+		log.Debug().Msg("Logging at a Debug level")
+	} else {
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+		log.Info().Msg("Logging at an Info level")
+	}
+
+	board.Set(0, 0, 'q')
+	let, err := board.Get(0, 0)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Output: %c", let)
 }
